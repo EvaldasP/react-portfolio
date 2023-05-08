00439ae6b51410c3307c87b31996596dffb01be1
Evaldas Pocius <evaldukas96@gmail.com>
2023-05-08 22:34:47 +0300

    initial particles background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import Particles from 'react-tsparticles';
 import './App.css';
+import { particlesConfig } from './utils/particlesConfig';
+import { useCallback } from 'react';
+import type { Engine } from 'tsparticles-engine';
+import { loadFull } from 'tsparticles';
 import Navbar from './components/Navbar';
 
 function App() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
   return (
     <div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={particlesConfig}
+        // Optional props
+        width="100vw"
+        height="100vh"
+      />
       <Navbar />
       <p>Hello, world!</p>
     </div>
---
+++ src/utils/particlesConfig.ts
@@ -0,0 +1,37 @@
+export const particlesConfig = {
+  background: {
+    color: {
+      value: '#0d47a1',
+    },
+  },
+  particles: {
+    color: {
+      value: '#ffffff',
+    },
+    links: {
+      color: '#ffffff',
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+};
